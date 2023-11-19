cd46dd64d2bcfab00727c9ebee5b27117bc91719
Viktoryia <manuelka.wikta@gmail.com>
2023-11-19 13:03:24 +0100

    create second draft folder

--- script.js
+++
@@ -1,9 +0,0 @@
-
-
-const mobileView = document.querySelector('.mobile-navigation ul');
-
-document.querySelector('.mobile-navigation i').addEventListener('click', ()=>{
-    mobileView.classList.contains('show') ? mobileView.remove('show') : mobileView.addEventListener('show')
-});
-
-ScrollReveal().reveal('skyscrip-row');
