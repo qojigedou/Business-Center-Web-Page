946c8d1b0bb6137dd7c9b442b82a57c1e3a13e73
Viktoryia <manuelka.wikta@gmail.com>
2023-11-19 15:54:13 +0100

    complete first draft

--- first_draft/script.js
+++ first_draft/script.js
@@ -3,7 +3,7 @@
 const mobileView = document.querySelector('.mobile-navigation ul');
 
 document.querySelector('.mobile-navigation i').addEventListener('click', ()=>{
-    mobileView.classList.contains('show') ? mobileView.remove('show') : mobileView.addEventListener('show')
+    mobileView.classList.contains('show') ? mobileView.classList.remove('show') : mobileView.classList.add('show')
 });
 
 ScrollReveal().reveal('skyscrip-row');
