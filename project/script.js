8b16a51843b8245e5ef47010afadda81603bb556
Viktoryia <manuelka.wikta@gmail.com>
2023-11-21 21:25:57 +0100

    add free place page

--- project/script.js
+++ project/script.js
@@ -12,3 +12,18 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 } 
+
+
+
+
+
+
+const listOfCardElements = document.querySelectorAll('.card');
+const cardContainer = document.querySelector('.card-container');
+
+listOfCardElements.forEach((cardElement, index) => {
+  cardElement.addEventListener('click', () => {
+    const scrollLeft = index * listOfCardElements[0].offsetWidth;
+    cardContainer.scrollTo({ left: scrollLeft, behavior: 'smooth' });
+  });
+});
