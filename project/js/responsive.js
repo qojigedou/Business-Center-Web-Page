a570db8b3bab089169b3219a2dcdc9259cd46e8f
Viktoryia <manuelka.wikta@gmail.com>
2023-11-25 15:46:48 +0100

    move drafts to drafts folder

--- project/js/responsive.js
+++ project/js/responsive.js
@@ -1,9 +1,6 @@
-
-
 const mobileView = document.querySelector('.mobile-navigation ul');
 
 document.querySelector('.mobile-navigation i').addEventListener('click', ()=>{
     mobileView.classList.contains('show') ? mobileView.classList.remove('show') : mobileView.classList.add('show')
 });
 
-ScrollReveal().reveal('skyscrip-row');
