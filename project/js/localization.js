27aca5d3c02e73c05e00f3d6900e4b635ae656f7
Viktoryia <manuelka.wikta@gmail.com>
2023-11-25 15:11:36 +0100

    add icons and get rid of trash

--- project/js/localization.js
+++ project/js/localization.js
@@ -1,3 +1,4 @@
 import appearOnScroll from "./appearOnScroll.js";
 
 const row = new appearOnScroll("skyscrap-row", "slide_from_left");
+const service_animation = new appearOnScroll("service-nearby", "slide_from_right")
